9a65c378f7ac6c3a5555569f167565206ccf9df9
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-04-23 15:48:58 -0300

    Test 1 - async jest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,12 +12,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
     // Insira seu teste assíncrono aqui
-    
+    expect.assertions(1);
+    return answerPhone(true).then(result =>
+      expect(result).toBe('Oi!'));
   });
   test('ocupado', () => {
-    assert.fail();
     // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(false).catch(result =>
+      expect(result).toEqual(new Error('Infelizmente não podemos atender...')));
   });
 });
