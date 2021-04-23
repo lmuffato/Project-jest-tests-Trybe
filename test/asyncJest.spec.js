ce6c7824cba2d2a3a4769c698e939c236adb2d67
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-04-23 15:33:37 -0300

    requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(true).then((string) => {
+      expect(string).toBe('Oi!');
+    });
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    await expect(answerPhone(false))
+      .rejects
+      .toThrow('Infelizmente não podemos atender...');
   });
 });
