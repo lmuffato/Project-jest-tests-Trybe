e4c2e401352342cbdce1aceb0f7986e89e0c2c72
Southier <patrick.southier@hotmail.com>
2021-04-23 19:21:39 -0300

    Completes requirement 2.

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,4 +1,5 @@
 const mockFunctions = require('../src/mockFunctions');
+jest.mock('../src/mockFunctions');
 
 /*
 Criamos uma série de funções com eficiência duvidosa.
@@ -15,7 +16,20 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+  beforeEach(() => {
+    mockFunctions.add.mockImplementation((a, b) => a + b);
+    mockFunctions.subtract.mockImplementation((a, b) => a - b);
+    mockFunctions.multiply.mockImplementation((a, b) => a * b);
+    mockFunctions.divide.mockImplementation((a, b) => a / b);
+    mockFunctions.power.mockImplementation((a, b) => Math.pow(a, b));
+    mockFunctions.factorial.mockImplementation((a) => {
+      if (a === 0 || a === 1) { return 1 }
+      for (let index = a - 1; index >= 1; index -= 1) {
+        a *= index;
+      }
+      return a;
+    });
+  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
