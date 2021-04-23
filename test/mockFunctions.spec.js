fed6713d8563573dee378447163adc22c927bf4c
mallmann02 <leomallmann020302@gmail.com>
2021-04-23 16:18:51 -0300

    Requirements - 2 (Create a mock of a module)

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -14,9 +14,21 @@ O foco aqui é a utilização de mock functions.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+jest.mock('../src/mockFunctions');
 
+describe('verifica as funções e os mocks', () => {
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation(a => {
+    let fact = 1;
+    for (let i = 1; i <= a; i += 1) {
+      fact *= i;
+    }
+    return fact;
+  })
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
