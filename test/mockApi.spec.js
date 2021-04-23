c42e72f002fb92fd5bd84de5f2c083af7d22754a
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-04-23 17:12:41 -0300

    Requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,8 +24,24 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const myUser = {
+    name: {
+      first: 'Antonio',
+      last: 'Britto',
+    },
+    gender: 'Male',
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
 
-  test('verifica se o usuário é o tunico', async () => (
+  test('verifica se o usuário é o tunico', async () => {
+    api.fetchURL = jest.spyOn(api, 'fetchURL').mockResolvedValue(myUser);
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
@@ -34,6 +50,6 @@ describe('verifica o usuário', () => {
       expect(user.email).toEqual('tunico@bol.com.br');
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
-    })
-  ));
+    });
+  });
 });
