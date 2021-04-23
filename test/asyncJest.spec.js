2a2d0b375753981a2b8aa5459c8273921993d10e
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-04-23 15:41:49 -0300

    Adicionando requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true).then((data) => {
+      expect(data).toEqual('Oi!');
+    });
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    return answerPhone(true).catch((erro) => {
+      expect(erro).toEqual('Infelizmente não podemos atender...');
+    });
   });
 });
