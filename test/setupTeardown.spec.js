6f30a81953da89e004ce842dfa9a488bc5efaff5
Iago de Paula <iago.pferreiravr@gmail.com>
2021-04-23 16:46:57 -0300

    Quarto requisito

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => adventure.randomAttack());
+  afterEach(() => console.log(adventure.specialists));
+  afterAll(() => console.log(`The only survivor of this massacre was ${adventure
+    .specialists[0].nome}`));
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
