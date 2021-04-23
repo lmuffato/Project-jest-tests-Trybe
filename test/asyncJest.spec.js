7c8fe91ac22fa4821f785edc6e7f5098fa20fd97
Rafael Ribeiro <rafittu@outlook.com>
2021-04-23 15:18:32 -0300

    requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect(answerPhone()).resolves.toBe('Oi!');
   });
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect(answerPhone()).rejects.toEqual('Infelizmente não podemos atender...');
   });
 });
