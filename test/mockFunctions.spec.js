a20b309fe09cedf2f8aa6af36f488ba62a28de35
mallmann02 <leomallmann020302@gmail.com>
2021-04-23 17:23:57 -0300

    Fix: eslint errors

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -22,13 +22,13 @@ describe('verifica as funções e os mocks', () => {
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
   mockFunctions.divide.mockImplementation((a, b) => a / b);
   mockFunctions.power.mockImplementation((a, b) => a ** b);
-  mockFunctions.factorial.mockImplementation(a => {
+  mockFunctions.factorial.mockImplementation((a) => {
     let fact = 1;
     for (let i = 1; i <= a; i += 1) {
       fact *= i;
     }
     return fact;
-  })
+  });
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
