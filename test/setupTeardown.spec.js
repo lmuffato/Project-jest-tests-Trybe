c9c770ca8cee967c2b6b991fb985b024c782bc2e
lucas-chave <lcchavesr150@gmail.com>
2021-04-23 20:28:09 -0300

    corrigindo

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -22,7 +22,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
   beforeEach(() => {
-    console.log(adventure.randomAttack());
+    adventure.randomAttack();
   });
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
