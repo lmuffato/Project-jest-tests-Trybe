da6970752e2863db139feecf575c586752c88e7e
carlos25xrm <carlos25xrm@gmail.com>
2021-04-23 17:54:38 -0300

    Corrigindo e adicionando novos comentários no Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,3 @@
-// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -25,15 +24,19 @@ describe('o retorno do telefonema', () => {
 });
 
 // OBSERVAÇÕES:
-// Utilizei o conteúdo do corse e esse link: https://jestjs.io/pt-BR/docs/asynchronous
+// Utilizei o conteúdo do course e esse link: https://jestjs.io/pt-BR/docs/asynchronous
 // para fazer e entender como esse teste funciona.
 
-// 13 - Descrição sobre o teste
-// 14 - Nome do teste + dizendo que o teste é assíncrono
-// 15 - Guarda dentro da constante a espera "true" da função
-// 16 - Mostra o esperado e compara se é igual ao que a função retorna
-// 18 - Nome do teste + dizendo que o teste é assíncrono
-// 19 - Abre um novo bloco de código para o erro
-// 20 - Aguarda para saber se a resposta da função é "false"
-// 21 - Capitura o erro informado pela função
-// 22 - Mostra o esperado e compara se é igual ao erro que a função retorna
+// Vou deixar comentado por linha para servir como
+// minha própria fonte de pesquisa em projetos futuros!
+
+// 12 - Descrição sobre o teste
+// 13 - Nome do teste + dizendo que o teste é assíncrono
+// 14 - Guarda dentro da constante a espera "true" da função
+// 15 - Mostra o esperado e compara se é igual ao que a função retorna
+// 17 - Nome do teste + dizendo que o teste é assíncrono
+// 18 - Abre um novo bloco de código relacionado ao caso de erro
+// 19 - Aguarda para saber se a resposta da função é "false"
+// 20 - Captura o erro informado pela função (New Error)
+// 21 - Mostra o esperado e compara se é igual ao "New Error" que a função retorna
+// Demais linhas são apenas o fechamento de } e ) nas funções
