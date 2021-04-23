d97c08d8842422716016cc92f6e510bf849abc5f
jonathan <jonathanfernandesjf98@gmail.com>
2021-04-23 18:43:47 -0300

    requisito 01

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -11,12 +11,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
+  test('atende', async () => {
     // Insira seu teste assíncrono aqui
+    await expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
+  test('ocupado', async () => {
     // Insira seu teste assíncrono aqui
+    await expect(answerPhone(false)).rejects
+      .toThrow();
+    await expect(answerPhone(false)).rejects
+      .toThrowError(new Error('Infelizmente não podemos atender...'));
   });
 });
+
+// Utilizei esses dois links para um entendimento mais completo da utilização do toThrow e toThrowError ''https://github.com/facebook/jest/issues/781'', ''https://jestjs.io/docs/expect#tothrowerror'';
