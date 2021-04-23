74992033a9dad9a1a2b27711502f6af822f79117
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-04-23 15:51:16 -0300

    feat(exercise 01): :rocket: Add tests

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -12,11 +12,20 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
+    expect.assertions(1);
+    return answerPhone('Alô').then((response) => {
+      expect(response).toBe('Oi!');
+    });
     // Insira seu teste assíncrono aqui
   });
-  test('ocupado', () => {
-    assert.fail();
+  test('ocupado', async () => {
     // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    try {
+      await answerPhone(undefined);
+    }
+    catch (error) {
+      expect(error).toEqual(new Error('Infelizmente não podemos atender...'));
+    }
   });
 });
