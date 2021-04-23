8bbc08ba028f4fba4d99edd2fd7136b9d8cc8d31
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-04-23 17:31:41 -0300

    req 1 refatorado

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,14 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', async () => {
+    expect.assertions(1)
     await expect(answerPhone(true)).resolves.toBe('Oi!');
   });
 
   test('ocupado', async () => {
-    try {
-      await answerPhone(false);
-    } catch (error) {
-      expect(error).toEqual(Error('Infelizmente não podemos atender...'));
-    }
-  });
-});
+    expect.assertions(1)
+    await expect(answerPhone(false)).rejects.toEqual(Error('Infelizmente não podemos atender...'));
+    });
+})
