960970c24afb6d6b95cc837da4e040d4cf7e387c
Marcelofcdantas <65692996+Marcelofcdantas@users.noreply.github.com>
2021-04-23 19:24:43 -0300

    lint corrigido

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,13 +23,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 const fetchingURL = {
   gender: 'male',
-  name: {first: 'Antônio',
-  last: 'Britto'
+  name: { first: 'Antônio',
+    last: 'Britto',
   },
   location: { country: 'Brazil' },
   email: 'tunico@bol.com.br',
   login: { username: 'tunicao123',
-  password: '1234567890',},
+    password: '1234567890' },
 };
 
 describe('verifica o usuário', () => {
