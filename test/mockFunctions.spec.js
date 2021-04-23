eb52421aff1723b27426c5aa19510451c2559d69
guidugaich <guidugaich.dev@gmail.com>
2021-04-23 17:18:08 -0300

    requisito 2 - mockando funcoes estranhas utilizando jest.mock para o modulo e mockImplementation para cada funcao

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -14,8 +14,21 @@ O foco aqui é a utilização de mock functions.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+jest.mock('../src/mockFunctions');
+
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => (a ** b));
+  // referencia: https://www.educative.io/edpresso/how-to-find-the-factorial-of-a-number-in-javascript
+  function factorial(a) {
+    if ((a === 0) || (a === 1)) return 1;
+    return a * factorial(a - 1);
+  }
+  mockFunctions.factorial.mockImplementation(factorial);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
