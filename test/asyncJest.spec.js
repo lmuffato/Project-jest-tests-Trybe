b96e632b9ea023ad2437d145f3515124518d81dc
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-23 15:47:04 -0300

    Requisito 1 - correçao do lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -18,7 +18,7 @@ describe('o retorno do telefonema', () => {
   test('ocupado', async () => {
     try {
       await answerPhone(false);
-    } catch(error) {
+    } catch (error) {
       expect(error).toEqual(Error('Infelizmente não podemos atender...'));
     }
   });
