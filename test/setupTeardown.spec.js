79fb8c6c8c3930c7fe87cc296a25448265d525fe
andy silva <andssilva.23@gmail.com>
2021-04-23 17:52:04 -0400

    feat(requisito 4): :rocket: cria funções no arquivo test/setupTeardown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,8 +20,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
-
+  beforeEach(() => adventure.randomAttack());
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
@@ -37,4 +36,5 @@ describe('quem sobreviveu?', () => {
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
   });
+  // afterEach(() => console.log(`${adventure.specialists.nome} sobreviveu! Parabéns!`));
 });
