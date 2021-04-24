514d96c9fdeda551ae0f7865c0507e688182af17
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-24 20:19:24 -0300

    melhoramento do código, implementação da maneira correta onde ocorre a chamando da função original porém ela já esta com o método spyOn implementado, podendo assim manipular o retorno

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -39,18 +39,18 @@ describe('verifica o usuário', () => {
       password: '1234567890',
     },
   };
-  const apiURL = jest.spyOn(api, 'fetchURL');
-  afterEach(apiURL.mockReset());
 
-  test('verifica se o usuário é o tunico', async () => {
-    apiURL.mockResolvedValue(usuario);
-    apiURL();
-    expect(usuario.gender).toEqual('male');
-    expect(usuario.name.first).toEqual('Antônio');
-    expect(usuario.name.last).toEqual('Britto');
-    expect(usuario.location.country).toEqual('Brazil');
-    expect(usuario.email).toEqual('tunico@bol.com.br');
-    expect(usuario.login.username).toEqual('tunicao123');
-    expect(usuario.login.password).toEqual('1234567890');
-  });
+  api.fetchURL = jest.spyOn(api, 'fetchURL').mockResolvedValue(usuario);
+
+  test('verifica se o usuário é o tunico', async () => (
+    api.fetchURL().then((user) => {
+      expect(user.gender).toEqual('male');
+      expect(user.name.first).toEqual('Antônio');
+      expect(user.name.last).toEqual('Britto');
+      expect(user.location.country).toEqual('Brazil');
+      expect(user.email).toEqual('tunico@bol.com.br');
+      expect(user.login.username).toEqual('tunicao123');
+      expect(user.login.password).toEqual('1234567890');
+    })
+  ));
 });
