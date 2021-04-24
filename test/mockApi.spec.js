22938cd4e73d325fce1a0f6a12c2d350f21c6480
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-23 22:57:46 -0300

    requirement "Crie um Mock para o retorno da API" refactored

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,23 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  // Mock da função fetchURL()
+  api.fetchURL = jest.fn().mockImplementation(async () => {
+    const obj = {
+      gender: 'male',
+      name: { first: 'Antônio', last: 'Britto' },
+      location: {
+        country: 'Brazil',
+      },
+      email: 'tunico@bol.com.br',
+      login: {
+        username: 'tunicao123',
+        password: '1234567890',
+      },
+    };
+
+    return obj;
+  });
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
