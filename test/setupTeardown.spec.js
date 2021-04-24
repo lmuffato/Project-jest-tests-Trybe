9f883aa4d72e5dbf95435a7f441715c291bdf903
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-24 00:26:47 -0300

    requirement "Crie funções no arquivo test/setupTeardown.spec.js"done

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
+
+  afterEach(() => {
+    if (adventure.specialists.length > 1) console.log('The survivors are:');
+    else console.log('The survivor...');
+    adventure.specialists.forEach((specialist) => {
+      console.log(`${specialist.nome}, the ${specialist.classe}`);
+    });
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
