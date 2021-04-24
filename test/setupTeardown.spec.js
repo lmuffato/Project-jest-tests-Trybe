c96aa66ec88c3f6b89040d5c3d658483de78f3d8
Marcelofcdantas <65692996+Marcelofcdantas@users.noreply.github.com>
2021-04-24 19:00:27 -0300

    lint corrigido

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -19,7 +19,7 @@ PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 describe('quem sobreviveu?', () => {
- beforeEach(() => adventure.randomAttack());
+  beforeEach(() => adventure.randomAttack());
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
