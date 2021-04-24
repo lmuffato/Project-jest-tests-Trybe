ec36981fed3cdff1b83779e211e2e03739d05545
luanlsr <luanfswd@gmail.com>
2021-04-24 16:35:45 -0300

    requisito 1 corrigido

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -9,14 +9,14 @@ a função recebe como parâmetro true e false, respectivamente.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-// código feito baseado na documentação do Jest, das aulas da turma 8 e 7 com os professores Oliva e Gustavo Caetano e do Pull Request do Vitor Canto 
+// código feito baseado na documentação do Jest, das aulas da turma 8 e 7 com os professores Oliva e Gustavo Caetano e do Pull Request do Vitor Canto
 
 describe('o retorno do telefonema', () => {
   it('deveria retornar o atendimento oi', async () => {
-      expect(answerPhone(true)).resolves .toBe('Oi!'); 
-  }); 
+      expect(answerPhone(true)).resolves .toBe('Oi!');
+  });
   it('deveria retornar um erro', async () => {
-    expect(answerPhone(false)).rejects 
-      .toThrow('Infelizmente não podemos atender...'); 
-  }); 
+    expect(answerPhone(false)).rejects
+      .toThrow('Infelizmente não podemos atender...');
+  });
 });
