f0d28778f7ebe0e4172a0fd3fd522ae2109486f7
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-04-24 16:12:38 -0300

    Fixing setupTeardown issue spotted by Julia Baptista

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -3,21 +3,21 @@ const adventure = require('../src/setupTeardown');
 describe('quem sobreviveu?', () => {
   beforeEach(() => adventure.randomAttack());
   afterEach(() => console.table(adventure.specialists));
-  afterAll(() => console.log(`O aventureiro ${adventure.specialists[0]} sobreviveu! 
+  afterAll(() => console.log(`O aventureiro ${adventure.specialists[0].nome} sobreviveu! 
 Fs in the chat for the fallen heroes`));
-  test('depois da primeira aventura', () => {
+  it('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
-  test('depois da segunda aventura', () => {
+  it('depois da segunda aventura', () => {
     expect(adventure.specialists.length).toBe(4);
   });
-  test('depois da terceira aventura', () => {
+  it('depois da terceira aventura', () => {
     expect(adventure.specialists.length).toBe(3);
   });
-  test('depois da quarta aventura', () => {
+  it('depois da quarta aventura', () => {
     expect(adventure.specialists.length).toBe(2);
   });
-  test('depois da quinta aventura', () => {
+  it('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
   });
 });
