86fcca5b044277c79d492f7177172bfa430068ec
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-24 17:25:26 -0300

    Requisito 3 lint com lint corrigido

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -37,8 +37,8 @@ describe('verifica o usuário', () => {
       coordinates: { latitude: '26.2451', longitude: '45.2995' },
       timezone: {
         offset: '+5:30',
-        description: 'Bombay, Calcutta, Madras, New Delhi'
-      }
+        description: 'Bombay, Calcutta, Madras, New Delhi',
+      },
     },
     email: 'tunico@bol.com.br',
     login: {
@@ -48,7 +48,7 @@ describe('verifica o usuário', () => {
       salt: 'XKOOGc2x',
       md5: '8cb7b4686f3869247b3ed189de780ea6',
       sha1: 'c24641f415cf36f4494ea4007fb3d77b47a6aad5',
-      sha256: 'a7bdd079ead0adf21f30cee5b94e5581a9fa0d5fc8b3c1881dbc864dabc55a80'
+      sha256: 'a7bdd079ead0adf21f30cee5b94e5581a9fa0d5fc8b3c1881dbc864dabc55a80',
     },
     dob: { date: '1965-10-01T06:35:49.694Z', age: 55 },
     registered: { date: '2009-02-11T05:48:39.772Z', age: 11 },
@@ -58,10 +58,10 @@ describe('verifica o usuário', () => {
     picture: {
       large: 'https://randomuser.me/api/portraits/women/7.jpg',
       medium: 'https://randomuser.me/api/portraits/med/women/7.jpg',
-      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/7.jpg'
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/7.jpg',
     },
-    nat: 'IE'
-  } ;
+    nat: 'IE',
+  };
 
   api.fetchURL = jest.fn().mockReturnValue(Promise.resolve(json));
 
