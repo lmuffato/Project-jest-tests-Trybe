98bed2b1f61f114d435f781ab191c39055238dee
Villefor <franklincrius@gmail.com>
2021-04-23 22:00:00 -0300

    2 requisito

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -22,8 +22,24 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+// referência = Material da trybe tinha uma questão igual, tive de buscar as informações da lista na API  da função
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const infoList = {
+    gender: 'male',
+
+    name: { first: 'Antônio', last: 'Britto' },
+
+    location: { country: 'Brazil' },
+
+    email: 'tunico@bol.com.br',
+
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+
+  api.fetchURL = jest.fn().mockResolvedValue(infoList);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
