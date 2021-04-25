93091c87eeade18a40755aac9cbd4b3ae44cb7f3
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-04-25 20:39:49 -0300

    requisito 2

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,5 +1,7 @@
 const api = require('../src/mockApi');
 
+jest.fn().mock('../src/mockApi');
+
 /*
 A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
 No entanto, nos testes abaixo, queremos que todas as vezes que chamarmos a API a resposta contenha as informações do nosso adminis..Cof! Cof!.. programador favorito, Tunicão.
@@ -23,9 +25,25 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  test('verifica se o usuário é o tunico', async () => {
+    const newValues = {
+      gender: 'male',
+      name: { first: 'Antônio', last: 'Britto' },
+      location: {
+        country: 'Brazil',
+        postcode: 16223,
+      },
+      email: 'tunico@bol.com.br',
+      login: {
+        username: 'unicao123',
+        password: '1234567890',
+      },
+    };
 
-  test('verifica se o usuário é o tunico', async () => (
+    api.mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(newValues),
+    }));
+
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
@@ -34,6 +52,6 @@ describe('verifica o usuário', () => {
       expect(user.email).toEqual('tunico@bol.com.br');
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
-    })
-  ));
+    });
+  });
 });
--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,5 +1,7 @@
 const mockFunctions = require('../src/mockFunctions');
 
+jest.mock('../src/mockFunctions');
+
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
@@ -15,7 +17,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) => {
+    let answer = 1;
+    for (let index = a; index >= 1; index -= 1) {
+      answer *= index;
+    }
+    return answer;
+  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
