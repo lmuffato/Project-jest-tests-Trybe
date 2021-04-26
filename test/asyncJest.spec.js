98b44c93392788583a32fa068c2f2f585c234542
Debora Passos <deborinhapassos@gmail.com>
2021-04-26 08:35:39 -0300

    Tentando fazer o evaluator funcionar

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,5 @@
 // const assert = require('assert');
+
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -7,7 +8,7 @@ Dependendo do parâmetro o retorno da função varia, veja a função no arquivo
 Complete o código abaixo para testar as situações em que
 a função recebe como parâmetro true e false, respectivamente.
 
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+ATENÇÃO!!! Edite apenas este arquivo. Não altere osgit sa arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
