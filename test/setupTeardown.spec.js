fb04686ce22c33e81753fa43f39cb3e8f24eb1e3
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-04-26 14:01:44 -0300

    Requisito 4: Adiçao do beforeEach para fazer o randomAttack e ir testando confome tests

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -22,6 +22,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
 
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
+
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
@@ -36,5 +40,9 @@ describe('quem sobreviveu?', () => {
   });
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
+  });
+
+  afterEach(() => {
+    console.log(adventure.specialists);
   });
 });
