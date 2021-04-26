33d50c9f2a6d7d6227ecc9da53937487044c31ad
carlos25xrm <carlos25xrm@gmail.com>
2021-04-26 11:54:18 -0300

    Refazendo(2) o Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -9,12 +9,14 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe('o retorno do telefonema', () => {
-  test('A chamada foi atendida', async () => {
-    const chamadaAtendida = await answerPhone(true);
-    expect(chamadaAtendida).toBe('Oi!');
+describe('Testes sobre a chamada', () => {
+  it('Chamada atendida (True)', async () => {
+    expect.assertions(1);
+    const data = await answerPhone(true);
+    expect(data).toBe('Oi!');
   });
-  test('A chamada está ocupada', async () => {
+  it('Chamada ocupada (False)', async () => {
+    expect.assertions(1);
     try {
       await answerPhone(false);
     } catch (error) {
@@ -24,19 +26,7 @@ describe('o retorno do telefonema', () => {
 });
 
 // OBSERVAÇÕES:
-// Utilizei o conteúdo do course e esse link: https://jestjs.io/pt-BR/docs/asynchronous
-// para fazer e entender como esse teste funciona.
-
-// Vou deixar comentado por linha para servir como
-// minha própria fonte de pesquisa em projetos futuros!
-
-// 12 - Descrição sobre o teste
-// 13 - Nome do teste + dizendo que o teste é assíncrono
-// 14 - Guarda dentro da constante a espera "true" da função
-// 15 - Mostra o esperado e compara se é igual ao que a função retorna
-// 17 - Nome do teste + dizendo que o teste é assíncrono
-// 18 - Abre um novo bloco de código relacionado ao caso de erro
-// 19 - Aguarda para saber se a resposta da função é "false"
-// 20 - Captura o erro informado pela função (New Error)
-// 21 - Mostra o esperado e compara se é igual ao "New Error" que a função retorna
-// Demais linhas são apenas o fechamento de } e ) nas funções
+// Revisei as aulas gravadas do course e esse
+// link: https://jestjs.io/pt-BR/docs/asynchronous
+// para e entender como o Jest funciona e fazer os requisitos.
+// (26/04/2021) - Decidi refazer o exercício depois de assistir as aulas.
