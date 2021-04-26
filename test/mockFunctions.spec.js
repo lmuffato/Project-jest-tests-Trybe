9b4796be222c749f8cd6c895bccfbae69f59838b
johnpligeiro <johnpligeiro@gmail.com>
2021-04-25 22:25:58 -0300

    Mock subtract, multiply, divide and power

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -16,6 +16,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   mockFunctions.add = jest.fn().mockImplementation((a, b) => a + b);
+  mockFunctions.subtract = jest.fn().mockImplementation((a, b) => a - b);
+  mockFunctions.multiply = jest.fn().mockImplementation((a, b) => a * b);
+  mockFunctions.divide = jest.fn().mockImplementation((a, b) => a / b);
+  mockFunctions.power = jest.fn().mockImplementation((a, b) => a ** b);
+  // mockFunctions.factorial = jest.fn().mockImplementation((a, b) => a ** b);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
