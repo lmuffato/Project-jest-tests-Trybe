d2435fb83d23ed4e7a34028558b454fbe2c9368c
rcalvs <rafaelcalvette@gmail.com>
2021-04-28 13:47:30 -0300

    fixed Lint Isseus

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,4 +1,5 @@
 const mockFunctions = require('../src/mockFunctions');
+
 jest.mock('../src/mockFunctions');
 
 /*
@@ -17,14 +18,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
-  mockFunctions.add = jest.fn().mockImplementation((a,b) => a + b);
-  mockFunctions.subtract = jest.fn().mockImplementation((a,b) => a - b);
-  mockFunctions.multiply = jest.fn().mockImplementation((a,b) => a * b);
-  mockFunctions.divide = jest.fn().mockImplementation((a,b) => a / b);
-  mockFunctions.power = jest.fn().mockImplementation((a,b) => a ** b);
-  mockFunctions.factorial = jest.fn().mockImplementation((a) => 
-    a === 0 || a === 1 ? 1 : a * mockFunctions.factorial(a - 1)
-  );
+  mockFunctions.add = jest.fn().mockImplementation((a, b) => a + b);
+  mockFunctions.subtract = jest.fn().mockImplementation((a, b) => a - b);
+  mockFunctions.multiply = jest.fn().mockImplementation((a, b) => a * b);
+  mockFunctions.divide = jest.fn().mockImplementation((a, b) => a / b);
+  mockFunctions.power = jest.fn().mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial = jest.fn().mockImplementation((a) => {
+    let factorial = a;
+    for (let index = 1; index < a; index += 1) {
+      factorial *= index;
+    }
+    return factorial;
+  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
