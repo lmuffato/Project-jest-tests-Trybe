39ef19132630dc5ada8515c86090d9056dcb6041
Roberto Natividade <robertonnv@gmail.com>
2021-04-29 08:55:48 -0300

    Requisitos

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    expect.assertion(1);
+    const resposta = await answerPhone(true);
+    expect(resposta).toStrictEqual('Oi!');
   });
-  test('ocupado', () => {
+  test('ocupado', async () => {
     assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone().catch((error) => {
+      expect(error.message).toStrictEqual('Infelizmente não podemos atender...');
+    });
   });
 });
--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
+
+  afterEach(() => {
+    console.log(adventure.specialists.name);
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
