e7d4c4316a047473f5b97d924a47b88898017c7c
Vinicios <soicinivt@gmail.com>
2021-04-30 01:52:00 -0300

    Medaforças Medapeças Medalutadores Medabots! Vamos, requisito 2.

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,17 +23,24 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
-
-  test('verifica se o usuário é o tunico', async () => (
-    api.fetchURL().then((user) => {
-      expect(user.gender).toEqual('male');
-      expect(user.name.first).toEqual('Antônio');
-      expect(user.name.last).toEqual('Britto');
-      expect(user.location.country).toEqual('Brazil');
-      expect(user.email).toEqual('tunico@bol.com.br');
-      expect(user.login.username).toEqual('tunicao123');
-      expect(user.login.password).toEqual('1234567890');
-    })
-  ));
+  api.fetchURL = jest.fn().mockReturnValue({
+    gender: 'male',
+    name: { first: 'Antônio',
+      last: 'Britto' },
+    location: {
+      country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123',
+      password: '1234567890' },
+  });
+  test('verifica se o usuário é o tunico', () => {
+    const user = api.fetchURL();
+    expect(user.gender).toEqual('male');
+    expect(user.name.first).toEqual('Antônio');
+    expect(user.name.last).toEqual('Britto');
+    expect(user.location.country).toEqual('Brazil');
+    expect(user.email).toEqual('tunico@bol.com.br');
+    expect(user.login.username).toEqual('tunicao123');
+    expect(user.login.password).toEqual('1234567890');
+  });
 });
