bfa88474e5b077a0db7efd7bdedb2a348bf866f8
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-30 00:04:40 -0300

    ajuste lint

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,13 +15,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  mockFunctions.multiply = jest.fn().mockImplementation((a,b) => a * b);
-  mockFunctions.power = jest.fn().mockImplementation((a,b) => a ** b);
+  mockFunctions.multiply = jest.fn().mockImplementation((a, b) => a * b);
+  mockFunctions.power = jest.fn().mockImplementation((a, b) => a ** b);
   mockFunctions.factorial = jest.fn().mockImplementation((a) => 
     a === 0 || a === 1 ? 1 : a * mockFunctions.factorial(a - 1));
-  mockFunctions.add = jest.fn().mockImplementation((a,b) => a + b);
-  mockFunctions.subtract = jest.fn().mockImplementation((a,b) => a - b);
-  mockFunctions.divide = jest.fn().mockImplementation((a,b) => a / b);
+  mockFunctions.add = jest.fn().mockImplementation((a, b) => a + b);
+  mockFunctions.subtract = jest.fn().mockImplementation((a, b) => a - b);
+  mockFunctions.divide = jest.fn().mockImplementation((a, b) => a / b);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
