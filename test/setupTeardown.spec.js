96216101a6e2711415b118590e33ff5120e60223
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-04-30 19:34:51 -0300

    finalizado

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -24,19 +24,19 @@ describe('quem sobreviveu?', () => {
     adventure.randomAttack();
   });
 
-  test('depois da primeira aventura', () => {
+  it('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
-  test('depois da segunda aventura', () => {
+  it('depois da segunda aventura', () => {
     expect(adventure.specialists.length).toBe(4);
   });
-  test('depois da terceira aventura', () => {
+  it('depois da terceira aventura', () => {
     expect(adventure.specialists.length).toBe(3);
   });
-  test('depois da quarta aventura', () => {
+  it('depois da quarta aventura', () => {
     expect(adventure.specialists.length).toBe(2);
   });
-  test('depois da quinta aventura', () => {
+  it('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
   });
 });
