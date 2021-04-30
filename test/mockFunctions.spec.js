d14e9f358781a71bfb5a087565a5c2202afa060c
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-29 23:39:28 -0300

    inclusao do requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,7 +15,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+  mockFunctions.multiply = jest.fn().mockImplementation((a,b) => a * b);
+  mockFunctions.power = jest.fn().mockImplementation((a,b) => a ** b);
+  mockFunctions.factorial = jest.fn().mockImplementation((a) => 
+    a === 0 || a === 1 ? 1 : a * mockFunctions.factorial(a - 1));
+  mockFunctions.add = jest.fn().mockImplementation((a,b) => a + b);
+  mockFunctions.subtract = jest.fn().mockImplementation((a,b) => a - b);
+  mockFunctions.divide = jest.fn().mockImplementation((a,b) => a / b);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
