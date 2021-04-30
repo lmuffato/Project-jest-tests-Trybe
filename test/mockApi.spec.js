547f227c45ce473d2d75b7ca1da96b6acf4a07c9
Tiago Santos <tiago881@gmail.com>
2021-04-29 22:56:51 -0300

    2 - Crie um "Mock" no arquivo

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -22,10 +22,26 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+const obj = {
+  gender: 'male',
+  name: {
+    first: 'Antônio',
+    last: 'Britto',
+  },
+  location: {
+    country: 'Brazil',
+  },
+  email: 'tunico@bol.com.br',
+  login: {
+    username: 'tunicao123',
+    password: '1234567890',
+  },
+};
 
-  test('verifica se o usuário é o tunico', async () => (
+describe('verifica o usuário', () => {
+  api.fetchURL = jest.spyOn(api, 'fetchURL').mockResolvedValue(obj);
+
+  test('verifica se o usuário é o tunico', async () =>
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
@@ -34,6 +50,5 @@ describe('verifica o usuário', () => {
       expect(user.email).toEqual('tunico@bol.com.br');
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
-    })
-  ));
+    }));
 });
