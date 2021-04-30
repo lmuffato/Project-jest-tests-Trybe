b4e1bd725f0f3fe91dd900f0fe07a307032765f5
Tiago Santos <tiago881@gmail.com>
2021-04-29 23:53:41 -0300

    Crie um Mock para o retorno da API

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -14,8 +14,21 @@ O foco aqui é a utilização de mock functions.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+const factorial = ((a) => {
+  if (a === 0) return 1;
+  return a * factorial(a - 1);
+});
+
+jest.mock('../src/mockFunctions');
+
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) => factorial(a));
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
