8fd8a7ab1ec4065a3c160fee403b768938fe0c9e
Douglas <douguinhodns@gmail.com>
2021-05-02 19:04:17 -0300

    lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+//const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -12,17 +12,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    //assert.fail();
+    // assert.fail();
     expect.assertions(1);
     return answerPhone(true).then((response) => {
-      expect(response).toEqual('Oi!')
-    })
+      expect(response).toEqual('Oi!');
+    });
   });
   test('ocupado', () => {
-    //assert.fail();
+    // assert.fail();
     expect.assertions(1);
     return answerPhone(false).catch((response) => {
-      expect(response).toEqual(new Error('Infelizmente não podemos atender...'))
-    })
+      expect(response).toEqual(new Error('Infelizmente não podemos atender...'));
+    });
   });
 });
