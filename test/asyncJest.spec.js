756ba715b31718b8b6ca6ec068d544d2c07aff0e
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-05-05 13:48:58 -0300

    Adiciona requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(true).then((response) => {
+      expect(response).toEqual('Oi!');
+    });
   });
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(false).catch((response) => {
+      expect(response).toEqual(new Error('Infelizmente não podemos atender...'));
+    });
   });
 });
