61890bc1c2475f2a83bfe3cc307aa39b0b073903
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-05-06 13:41:44 -0300

    enviando o requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,22 +1,21 @@
 const mockFunctions = require('../src/mockFunctions');
 
-/*
-Criamos uma série de funções com eficiência duvidosa.
-Elas estão no arquivo 'src/mockFunctions.js'.
-Crie mock functions para cada uma das operações de modo que os cálculos sejam feitos corretamente,
-não como estão sendo feitos no arquivo original.
-A idéia é que os novos testes sobrescrevam os testes
-importados apenas na suite de testes abaixo.
-
-Importante! A correção de código via mock functions não é uma aplicação usual.
-O foco aqui é a utilização de mock functions.
-
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
-*/
+jest.mock('../src/mockFunctions');
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
-
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) => {
+    let acc = 1;
+    for (let i = 1; i <= a; i += 1) {
+      acc *= i;
+    }
+    console.log(acc);
+    return acc;
+  });
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
@@ -60,3 +59,17 @@ describe('verifica as funções e os mocks', () => {
     expect(mockFunctions.factorial(2)).toEqual(2);
   });
 });
+
+/*
+Criamos uma série de funções com eficiência duvidosa.
+Elas estão no arquivo 'src/mockFunctions.js'.
+Crie mock functions para cada uma das operações de modo que os cálculos sejam feitos corretamente,
+não como estão sendo feitos no arquivo original.
+A idéia é que os novos testes sobrescrevam os testes
+importados apenas na suite de testes abaixo.
+
+Importante! A correção de código via mock functions não é uma aplicação usual.
+O foco aqui é a utilização de mock functions.
+
+ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+*/
