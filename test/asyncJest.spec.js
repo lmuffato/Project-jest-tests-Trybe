dd617ac89d694bc200ebb85eaf0a2b9b335f624e
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-05-10 19:13:57 -0300

    Adicionando teste async.Jest.spec.js

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,16 +10,19 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    expect.assertions(1);
+  test('atende', async () => {
+    const atende = await answerPhone(true);
+    expect(atende).toStrictEqual('Oi!');
+    /* expect.assertions(1);
     return answerPhone(true).then((response) => {
       expect(response).toStrictEqual('Oi!');
-    });
+    }); */
   });
   test('ocupado', () => {
-    expect.assertions(1);
+    expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
+    /* expect.assertions(1);
     return answerPhone(false).catch((response) => {
       expect(response).rejects.toEqual('Infelizmente não podemos atender...');
-    });
+    }); */
   });
 });
