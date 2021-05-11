446b0f03b76dc2ed403c648b458f4a4177ade89a
Lucas Muffato <lmiremuffato@gmail.com>
2021-05-11 11:08:23 -0300

    requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -49,7 +49,7 @@ describe('verifica as funções e os mocks', () => {
     expect(mockFunctions.divide(1331, 11)).toEqual(121);
   });
   test('testa função power', () => {
-    mockFunctions.power = jest.fn().mockImplementation((a ,b) => a ** b);
+    mockFunctions.power = jest.fn().mockImplementation((a, b) => a ** b);
     expect(mockFunctions.power(10, 2)).toEqual(100);
     expect(mockFunctions.power(2, 10)).toEqual(1024);
     expect(mockFunctions.power(5, 5)).toEqual(3125);
