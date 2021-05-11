72823d0c2705a220a66642efd4c5cea5fb0d5efa
Lucas Muffato <lmiremuffato@gmail.com>
2021-05-11 10:09:56 -0300

    requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    const atendido = await answerPhone(true);
+    expect(atendido).toEqual('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  it('ocupado', async () => {
+    try { await answerPhone(false); }
+    catch (error) {
+      expect(error).toStrictEqual(Error('Infelizmente não podemos atender...'));
+    }
   });
 });
