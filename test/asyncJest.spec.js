7a9880adc561d64d741690505098c2b8146c0bd4
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-05-13 14:23:40 -0300

    lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-//const assert = require('assert');
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -16,8 +16,8 @@ describe('o retorno do telefonema', () => {
     return answerPhone(true).then((answer) => {
       expect(answer).toEqual('Oi!');
     });
-  }); 
-  
+  });
+
   test('ocupado', async () => {
     expect.assertions(1);
     try {
