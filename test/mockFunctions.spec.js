abb585d66ec9a017019e3e973175b033b54f6914
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-05-13 14:43:58 -0300

    lint

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -28,7 +28,7 @@ describe('verifica as funções e os mocks', () => {
   mockFunctions.divide.mockImplementation((a, b) => a / b);
   mockFunctions.power.mockImplementation((a, b) => a ** b);
   mockFunctions.factorial.mockImplementation((a) => factorial(a));
-  
+
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
