6ac3f998ba3a8eaf368bf64e6381337498dd53fe
Arlesson Moura <arlessonmss@gmail.com>
2021-05-13 15:50:09 -0300

    Requirement number one

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true).then((r) => {
+      expect(r).toEqual('Oi!');
+    });
   });
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(false).catch((err) => {
+      expect(err).toEqual(new Error('Infelizmente não podemos atender...'));
+    });
   });
 });
