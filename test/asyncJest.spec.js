8f6edfac8d26c5f4843a88b1b48094ae89601125
akirakiraa <andretaki2@gmail.com>
2021-05-14 01:58:32 -0300

    mockfunctions & async , lint to fix

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,22 +1,28 @@
-const assert = require('assert');
-const answerPhone = require('../src/asyncJest');
+
+const answerPhone = require("../src/asyncJest");
+
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
-
 Complete o código abaixo para testar as situações em que
 a função recebe como parâmetro true e false, respectivamente.
-
+Vamo que vamo!
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
+describe("o retorno do telefonema", () => {
+  test("atende", () => {
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    return answerPhone(true).then(response => {
+      expect(response).toBe('Oi!');
+    });
   });
-  test('ocupado', () => {
-    assert.fail();
+  test("ocupado", () => {
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    return answerPhone(false).catch(error => {
+      expect(error).toBe('Infelizmente não podemos atender...');
+    });
   });
 });
