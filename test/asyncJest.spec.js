ac3a88718a3f49bbed4fbac01c72679cfc13eefb
akirakiraa <andretaki2@gmail.com>
2021-05-14 03:04:35 -0300

    how to kill a mocking bird / lint to be fix

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,25 +1,22 @@
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
-
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
 Complete o código abaixo para testar as situações em que
 a função recebe como parâmetro true e false, respectivamente.
-Vamo que vamo!
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () =>
+  test('atende', () => {
     // assert.fail();
     // Insira seu teste assíncrono aqui
-    answerPhone(true).then((response) => {
-      expect(response).toBe('Oi!');
-    }));
-  test('ocupado', () =>
+    return expect(answerPhone(true)).resolves.toEqual('Oi!');
+  });
+  test('ocupado', () => {
     // assert.fail();
     // Insira seu teste assíncrono aqui
-    answerPhone(false).catch((error) => {
-      expect(error).toBe('Infelizmente não podemos atender...');
-    }));
+    return expect(answerPhone(false)).rejects.toEqual(Error('Infelizmente não podemos atender...'))
+  });
 });
