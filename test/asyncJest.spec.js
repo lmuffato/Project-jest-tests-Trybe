9938868193723a7954884420c1ce45511b18f342
Junior Portela <portelajr14@gmail.com>
2021-05-13 23:23:45 -0300

    implementado test funçao answerPhone

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -12,11 +11,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true).then((data) => {
+      expect(data).toEqual('Oi!');
+    });
   });
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(false).catch((e) => {
+      expect(e).toEqual(new Error('Infelizmente não podemos atender...'));
+    });
   });
 });
